07d79635795bf1c8a481779a8097436c9b561fe4
BerlinTokyo <dayzixe@gmail.com>
2021-03-14 14:40:09 +0000

    x

--- lab_2_task_2.py
+++ lab_2_task_2.py
@@ -1,10 +1,13 @@
 print('Hello students')
 print('Сompose a geometric progression')
 print('Enter the first term of the geometric progression')
-a=int(input())
+b1=int(input())
 print('Enter the denominator of the progression')
-b=int(input())
-print('Indicate the number of members of the progression, an integer')
-c=int(input())
-w=a*b*c
-print(w)
+q=int(input())
+print('Indicate the number of members of the progression, an integer, no more than 10')
+n=int(input())
+l = []
+for i in range(0, n):
+  b1 *=q
+  l.append(b1)
+print(l)
