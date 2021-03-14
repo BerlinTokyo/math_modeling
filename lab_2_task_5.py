e9b080dab111ecce7820dc0f9fa07e6968296d3f
BerlinTokyo <dayzixe@gmail.com>
2021-03-14 18:36:25 +0000

    x

--- lab_2_task_5.py
+++ lab_2_task_5.py
@@ -1,7 +1,17 @@
 print('Hello, Enter two integers')
 a=int(input())
 b=int(input())
-if u=a/b:
-  
-print(u)
+if a%b==0:
+  print("Первое число делится на второе")
+  print(a//b)
+elif b==0:
+  print('Ошибка, делитель равен 0')
+else:
+  print("Первое число не делится на второе")
+  print('Целая часть')
+  print(a//b)
+  print('Остаток')
+  print(a%b)
+print('The end')
+
 
