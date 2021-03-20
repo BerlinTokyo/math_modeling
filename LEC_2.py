f1d5375149f8e1102d031e1dc5e4bd8528d5ca76
BerlinTokyo <dayzixe@gmail.com>
2021-03-20 14:08:41 +0000

    q

---
+++ LEC_2.py
@@ -0,0 +1,15 @@
+a = [x for x in range(0, 10, 1)]
+print(a)
+b = []
+b.append(12)
+print(b)
+a= [x*0 for x in range(0, 10, 1)]
+print(a)
+b = tuple(a)
+print(a,b)
+c = list(b)
+print(c)
+a = {'1':'a','a':1,'b':[1,2,3]}
+print(a['1'])
+b=list(a)
+print(b)
