743c53cda8343620d27d3957a05d7c1ec4373cda
BerlinTokyo <>
2021-04-18 21:20:46 +0000

    q

--- lab_3_task_5.py
+++ lab_3_task_5.py
@@ -9,3 +9,19 @@ for i in range(0, N, 1):
       A[i, j] = np.sin(N*i+M*j)
     else:
       A[i, j] = np.sin(N*(i+1)+M*(j + 1))
+print('Введите слолбик кторый хотите поменять местами:')
+a=int(input())
+print('Введите слолбик кторый хотите поменять местами:')
+b=int(input())
+
+m=np.zeros((N))
+n=np.zeros((N))
+print(A)
+for i in range(0, N):
+  m[i] = A[i,a]
+  n=[i] = A[i,b]
+for i in range(0, N):
+  A[i, a] = n[i]
+  A[i, b]=m[i]
+print(A)
+
